b04f7bdf2d7e23071023f9524c290be37a905c19
fae2day <aqiu@andrew.cmu.edu>
2013-01-25 00:02:05 -0500

    Update items.js
    added basic item functionality 

--- items.js
+++ items.js
@@ -2,4 +2,32 @@
  * 		Contains item-related functions.
 */
 
-// NOT YET IMPLEMENTED
+function itemAction(){
+  
+  if(arguments[0]==undefined)
+		pickup(arguments[0]);
+	else{
+		switchitem(arguments[0],arguments[1]);
+	};
+}
+
+function pickUp(item){
+  
+	item.x=inventory.x;
+	item.y=inventory.y;
+	itemDialogue=("you have picked up "+item.name+"!");//this could be randomized
+  // dialogue should be made into a new js file. some other time.
+}
+
+function switchitem(onHand,newItem){
+	onHand.x=newItem.x;
+	onHand.y=newItem.y;
+	newItem.x=inventory.x;
+	newItem.y=inventory.y;
+  
+	itemDialogue=("You dropped "+onHand.name+" and picked up "+newItem.name+"!");
+}
+
+function itemDialogue(dialogue){
+
+}
