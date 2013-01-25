d33c8ba60a7c03dc3ed4a9aefef079e90819c386
fae2day <aqiu@andrew.cmu.edu>
2013-01-24 23:49:42 -0500

    Update items.js
    adding some functions that may be needed + what they should basically do

--- items.js
+++ items.js
@@ -2,4 +2,29 @@
  * 		Contains item-related functions.
 */
 
-// NOT YET IMPLEMENTED
+function action(){
+  if(arguments.length===0)
+    pickUp(arguments[0]);
+  else
+    drop(arguments[0],arguments[1]);
+
+}
+
+function pickUp(id){
+  idpick.x=inventory.x; // inventory is a variable
+  idpick.y=inventory.y;
+  dialogue("You picked up "+id);
+}
+
+function drop(idpick,iddrop){
+  iddrop.x=idpick.x;
+  iddrop.y=idpick.y;
+  idpick.x=inventory.x;
+  idpick.y=inventory.y;
+  
+  dialogue("You dropped "+idpick.name+" and picked up "+iddrop.name);
+}
+
+function dialogue(){
+  
+}
